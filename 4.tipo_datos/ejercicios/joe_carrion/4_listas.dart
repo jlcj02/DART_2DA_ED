0f571924deb46838d226652d912760bd51182d88
JLCJ <jlcj02@gmail.com>
2024-05-02 19:50:36 -0500

    #test

--- 4.tipo_datos/ejercicios/joe_carrion/4_listas.dart
+++ 4.tipo_datos/ejercicios/joe_carrion/4_listas.dart
@@ -22,4 +22,13 @@ final valor2 = lista.remove(60);
 print(valor2);
 
 
+ final list5 = [1, 2, 3, 4, 5];
+  print(list5.length); // 5
+  print(list5.first); // 1
+  print(list5.last); // 5
+  print(list5.reversed); // (5, 4, 3, 2, 1)
+  print(list5.isEmpty); // false
+  print(list5.isNotEmpty); // true
+  print(list5.reversed); // (5, 4, 3, 2, 1)
+
 }
